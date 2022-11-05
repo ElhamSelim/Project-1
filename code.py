7e2c6077d36a80ff787326044254d82202128660
Elham Selim <99829893+ElhamSelim@users.noreply.github.com>
2022-11-05 16:09:11 -0300

    Mycode

---
+++ code.py
@@ -0,0 +1,12 @@
+#Definitions of fruits in an array
+fruits = ['Banana','Apple','red','Pineapple','Strawberry','Grape']
+#continue --> skip the loop and continue 
+for fruit in fruits:
+  if fruit == 'red':
+    continue
+  print(fruit)
+#break --> and exist the loop
+for fruit in fruits:
+  if fruit == 'red':
+    break
+  print(fruit
